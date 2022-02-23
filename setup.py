89fcfb45789f07778ac570e98629a6b767913425
Alex de Aráujo (Americas) <alex.araujo@global.ntt>
2022-02-23 19:50:11 -0300

    Bump version: 0.1.0-beta.2 → 0.1.0-beta.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="detect-languages",
-    version="0.1.0-beta.2",
+    version="0.1.0-beta.3",
     keywords=("detect", "programming", "languages"),
     url="https://github.com/alexgracianoarj/detect-languages",
     license="MIT",

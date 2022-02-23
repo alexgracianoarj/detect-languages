1ca4d1f4e0294dd86f56e7df0a91b0ebd62f2007
Alex de Aráujo (Americas) <alex.araujo@global.ntt>
2022-02-23 19:47:46 -0300

    Update docstrings

--- detect_languages/detect.py
+++ detect_languages/detect.py
@@ -10,23 +10,22 @@ from detect_languages.languages import name_type_extensions
 
 
 class DetectLanguages:
-    def __init__(
-        self,
-        debug: bool = False,
-        path: str = ".",
-        language_types: List[str] = ["programming", "prose", "data", "markup"],
-        exclude_dirs: List[str] = [],
-        exclude_dirs_recursively: bool = False,
-    ) -> None:
-        """Instance and execute the analysis.
+    def __init__(self, debug: bool = False, path: str = ".", language_types: List[str] = None, exclude_dirs: List[str] = None, exclude_dirs_recursively: bool = False) -> None:
+        """
+        The function takes in a path to a directory and analyzes the files in that directory.
 
         Args:
-            debug (bool, optional): Show debug info. Defaults to False.
-            path (str, optional): Path to project or repository. Defaults to ".".
-            language_types (List[str], optional): Language types. Defaults to ["programming", "prose", "data", "markup"].
-            exclude_dirs (List[str], optional): Exclude dirs. Defaults to [].
-            exclude_dirs_recursively (bool, optional): Exclude dirs recursively. Defaults to False.
+          debug (bool): If True, the logging level will be set to DEBUG. Otherwise, it will be set to None.
+        Defaults to False
+          path (str): The path to the directory to analyze. Defaults to .
+          language_types (List[str]): A list of language types to include in the analysis.
+          exclude_dirs (List[str]): A list of directories to exclude from the analysis.
+          exclude_dirs_recursively (bool): If True, then exclude directories recursively. Defaults to False
         """
+        if language_types is None:
+            language_types = ["programming", "prose", "data", "markup"]
+        if exclude_dirs is None:
+            exclude_dirs = []
         logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.DEBUG if debug else None)
         self.__language_types = []
         self.__language_types.extend(language_types)
@@ -41,10 +40,12 @@ class DetectLanguages:
         self.__run_analysis(path)
 
     def __run_analysis(self, path: str) -> None:
-        """Run analysis.
+        """
+        Detects the language of the files in the given path and returns the main language and all the
+        detected languages
 
         Args:
-            path (str): Path to project or repository.
+          path (str): The path to the directory that contains the files to be analyzed.
         """
         logging.debug("Detecting languages...")
         logging.debug(f"Language types: {self.__language_types}")
@@ -60,31 +61,35 @@ class DetectLanguages:
         logging.debug(f"Detected languages: {list(self.all_languages.keys())}")
 
     def __filter_language_types(self) -> None:
-        """Filter extensions by language types."""
+        """
+        * Filter the language types by the ones we want to use
+        """
         for language_type in self.__language_types:
             extensions = jmespath.search(f"[?type=='{language_type}'].extensions[]", name_type_extensions())
             self.__language_types_extensions.extend(extensions)
 
     def __is_in_language_types(self, language: str) -> bool:
-        """Check if language is in language types.
+        """
+        Given a language, return True if the language is in the language_types list
 
         Args:
-            language (str): Language to check.
+          language (str): The language to check.
 
         Returns:
-            bool: Returns a boolean value.
+          The boolean value.
         """
         language_type = jmespath.search(f"[?name=='{language}'].type | [0]", name_type_extensions())
         return language_type in self.__language_types
 
     def __guess_file_language(self, file: str) -> str:
-        """Guess file language using pygments.
-
+        """
+        Guess the language of a file
+        
         Args:
-            file (str): File path.
-
+          file (str): The file to be analyzed.
+        
         Returns:
-            str: Returns language.
+          The language of the file.
         """
         try:
             with open(file, "r", encoding="utf-8") as reader:
@@ -100,10 +105,11 @@ class DetectLanguages:
             return
 
     def __filter_exclude_dirs(self, path: str) -> None:
-        """Filter exclude dirs on root level or recursively.
-
+        """
+        Iterate through all files in the given path and check if the file is a valid file.
+        
         Args:
-            path (str): Path to project or repository.
+          path (str): The path to the directory you want to search.
         """
         root, subdirs, files = next(os.walk(path))
         for filename in files:
@@ -122,10 +128,11 @@ class DetectLanguages:
                     self.__filter_exclude_dirs(subdir_path)
 
     def __find_files(self, subdir_path: str) -> None:
-        """Find files to check.
-
+        """
+        Find all files in the specified directory and subdirectories.
+        
         Args:
-            subdir_path (str): Subdirectory path.
+          subdir_path (str): The path to the directory that contains the files to be checked.
         """
         for root, _, files in os.walk(subdir_path):
             for filename in files:
@@ -137,12 +144,17 @@ class DetectLanguages:
                 self.__check_file(file, file_extension, file_size)
 
     def __check_file(self, file: str, file_extension: str, file_size: int) -> None:
-        """Check the file extension and language type.
-
+        """
+        Check if the file extension is in the language types extensions. If not, guess the language type of
+        the file. 
+        If the language type is not in the language types, return. 
+        If the language type is in the language types, add the file size to the total size of the language
+        type.
+        
         Args:
-            file (str): File path.
-            file_extension (str): File extension.
-            file_size (int): File size.
+          file (str): The file to check.
+          file_extension (str): The file extension of the file.
+          file_size (int): The size of the file in bytes.
         """
         if not file_extension or file_extension in self.__language_types_extensions:
             language = self.__guess_file_language(file)
@@ -157,11 +169,13 @@ class DetectLanguages:
             self.__add_file_size_and_total_files_size(language, file_size)
 
     def __add_file_size_and_total_files_size(self, language: str, size: int) -> None:
-        """Add file size and total files size.
-
+        """
+        Add the size of the file to the size of the language in the analysis and add the size of the file to
+        the total size of all files
+        
         Args:
-            language (str): Language to add.
-            size (int): File size.
+          language (str): The language of the file.
+          size (int): The size of the file in bytes.
         """
         if language in self.__analysis:
             self.__analysis[language]["size"] += size
@@ -170,6 +184,8 @@ class DetectLanguages:
         self.__total_files_size += size
 
     def __percentage_calculation(self) -> None:
-        """Percentage calculation"""
+        """
+        Calculate the percentage of each language in the directory
+        """
         for language in self.__analysis.keys():
             self.__analysis[language]["percentage"] = round(((self.__analysis[language]["size"] / self.__total_files_size) * 100), 2)
